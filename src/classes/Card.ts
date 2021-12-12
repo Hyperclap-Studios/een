942d21627ebe10ad6f24f752c58a784fe0c44aeb
Dan Photon <danphoton@yahoo.com>
2021-12-12 23:00:59 +0100

    A Little Bit Of Tomfoolery

--- src/classes/Card.ts
+++ src/classes/Card.ts
@@ -14,7 +14,8 @@ type CardValue =
     | '+4'
     | 'reverse'
     | 'skip'
-    | 'pickColor';
+    | 'pickColor'
+    | '+100';
 type CardState = 'deck' | 'played';
 
 class Card {
@@ -72,6 +73,9 @@ class Card {
             case '+4':
                 numericalValue = 14;
                 break;
+            case '+100':
+                numericalValue = 15;
+                break;
             default:
                 numericalValue = parseInt(this.value);
         }
@@ -102,7 +106,7 @@ class Card {
 
     private static randomValue(color: CardColor): CardValue {
         const valuesBasic: Array<CardValue> = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+2', 'reverse', 'skip'];
-        const valuesBlack: Array<CardValue> = ['+4', 'pickColor'];
+        const valuesBlack: Array<CardValue> = ['+4', 'pickColor', '+100'];
         if (color === 'black') return valuesBlack[Math.floor(Math.random() * valuesBlack.length)];
         else return valuesBasic[Math.floor(Math.random() * valuesBasic.length)];
     }
